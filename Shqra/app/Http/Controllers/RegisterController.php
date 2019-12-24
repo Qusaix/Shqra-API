da63411e7873ebb24750bb8ee359759a7fb7d663
qusaix <qusaiznemat6@gmail.com>
2019-12-24 15:14:19 +0200

    rigster and JWT Is Ready

---
+++ Shqra/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    protected function create(Request $data)
+    {
+        #return $data;
+        $user = new User;
+        $user->name = $data->name;
+        $user->email = $data->email;
+        $user->phone = $data->phone;
+        $user->zip_code = $data->zip_code;
+        $user->password = Hash::make($data['password']);
+        $user->save();
+
+        return response()->json(['success:User Was Created'],201);
+        
+    }
+}
--- Shqra/routes/api.php
+++ Shqra/routes/api.php
@@ -1,27 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group([
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::namespace('api')->group(function(){
-
-    // Users Route
-    Route::namespace('user')->prefix('user')->group(function(){
-        Route::post('/','RigsterController@signup');
-    });
+], function ($router) {
+    Route::post('rigster', 'RegisterController@create');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 
 });
+
