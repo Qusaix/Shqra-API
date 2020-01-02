d2509891750d1d130ecd66b44ab8b070c2bc18a5
Qusaix <qsuaiznemat6@gmail.com>
2020-01-02 09:54:45 -0800

    Fixed

--- Shqra/app/Http/Controllers/frontend/HomeController.php
+++ Shqra/app/Http/Controllers/frontend/HomeController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Categores;
-use App\ADS;
+use App\Ads;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $categores = Categores::get();
-        $ads = ADS::find(1);
+        $ads = Ads::find(1);
 
         return view('index',compact('categores','ads'));
     }
